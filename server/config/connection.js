0dddd513cd34cf6aa0eedaca78da96f0ca118a60
TPerschon <timperschon@gmail.com>
2022-09-22 20:48:38 -0600

    Resolvers/typeDefs start, connection streamline

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,9 @@
 // Import mongoose and set up a connection to our DB
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/campaign-tracker', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-module.exports = mongoose.connection;
+export default mongoose.connection;
