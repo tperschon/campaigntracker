3f550d3423d62e3ed15e950168204c6b0a782623
Jayden Griffith <griffithjayden97@outlook.com>
2022-09-24 10:20:58 -0600

    Added some content to the App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,42 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './css/App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
