bd3c41c1af3c3c1c5645daa93d58ce6b6348a86a
TPerschon <timperschon@gmail.com>
2022-09-27 20:22:47 -0600

    Validator message

--- server/models/User.js
+++ server/models/User.js
@@ -23,6 +23,7 @@ const userSchema = new Schema({
       validator: function(v) {
         return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])$/.test(v);
       },
+      message: 'Password must have at least: 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character.'
     }
   },
   campaigns: {
