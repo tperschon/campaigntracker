d07957ae8833b40f4932116b823e8b0a22babe14
TPerschon <timperschon@gmail.com>
2022-09-30 14:21:54 -0600

    Removed/renamed some components

--- client/src/components/CharacterList/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function CharacterList() {
-  return <div>CharacterList</div>;
-}
-
-export default CharacterList;
---
+++ client/src/components/PlayerList/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function PlayerList() {
+  return <div>PlayerList</div>;
+}
+
+export default PlayerList;
