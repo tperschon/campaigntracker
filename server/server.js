94e874256bc2543d26edf51f6946f71b056b64a7
TPerschon <timperschon@gmail.com>
2022-10-01 04:48:05 -0600

    *

--- server/server.js
+++ server/server.js
@@ -21,14 +21,12 @@ app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
-  console.log('prod')
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
